2e819358e197fc69270286f0d572cd506704f9fd
ravi2612 <ravinavarro26@gmail.com>
2021-01-02 23:00:35 -0300

    apli convert

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,29 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
   double dolar;
   double euro;
+  
+
+  void _realChanged(String text){
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
+  }
+  void _dolarChanged(String text){
+    double dolar = double.parse(text);
+    realController.text = (dolar*this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar*this.dolar / euro ).toStringAsFixed(2);
+  }
+  void _euroChanged(String text){
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,11 +105,11 @@ class _HomeState extends State<Home> {
                   children: <Widget> [
                     Icon(Icons.monetization_on, size: 150, color: Colors.amber, ),
 
-                    buildTextField('Reais', 'R\$'),
+                    buildTextField('Reais', 'R\$', realController, _realChanged),
                     Divider(),
-                    buildTextField('Dólares', 'US\$'),
+                    buildTextField('Dólares', 'US\$', dolarController, _dolarChanged),
                     Divider(),
-                    buildTextField('Euros', '€'),                  
+                    buildTextField('Euros', '€', euroController, _euroChanged),                  
                                         
                   ],
                 ),
@@ -103,8 +124,9 @@ class _HomeState extends State<Home> {
 }
 
 
-Widget buildTextField(String label, String prefix){
+Widget buildTextField(String label, String prefix, TextEditingController c, Function f){
   return TextField(
+                      controller: c,
                       decoration: InputDecoration(
                         labelText: label,
                         labelStyle: TextStyle(color: Colors.amber),
@@ -115,5 +137,7 @@ Widget buildTextField(String label, String prefix){
                         color: Colors.amber,
                         fontSize: 25.0,
                       ),
+                      onChanged: f,
+                      keyboardType: TextInputType.number,
                     );
 }
